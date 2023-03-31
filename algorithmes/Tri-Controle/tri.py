f0a20520284ded305eeb71f4aa226fd27dd50638
Arthur Lima <mozart.bachh@gmail.com>
2023-03-31 15:08:26 +0200

    Adding last modifications
    Modifying the tri.py file.

--- algorithmes/Tri-Controle/tri.py
+++ algorithmes/Tri-Controle/tri.py
@@ -39,12 +39,30 @@ def tri_ameliore(ta):
                 echange_valeur = True
     return {"list": ta, "comp": n_comp, "echange": n_echange}
 
-def comparer_tri(ameliore, bulles, list):
-    print(" ")
+def comparer_tri(ameliore, bulles, lst):
+    dic_ameliorer = ameliore(list(lst))
+    dic_bulles = bulles(list(lst))
 
-list = [5, 4, 7, 9, 1, 3, 2]
+    if (dic_ameliorer['echange'] < dic_bulles['echange']) or (dic_ameliorer['echange'] == dic_bulles['echange'] and dic_ameliorer['comp'] < dic_bulles['comp']):
+        dic_ameliorer['name'] = 'Tri amelioré'
+        format_result(dic_ameliorer, dic_bulles)
+        
+    else:
+        dic_bulles['name'] = 'Tri à bulle'
+
+        format_result(dic_bulles, dic_bulles)
+
+def format_result(dic, m_dic):
+    print('\n -=- LE PLUS EFFICIENT -=-\n')
+    print(f"Tri: {dic['name']}")
+    print(f"Numero d'echanges: {dic['echange']} (l'autre tri: {m_dic['echange']})")
+    print(f"Numero de comparaison: {dic['comp']} (l'autre tri: {m_dic['comp']})")
+
+
+lst = [5, 4, 7, 9, 1, 3, 2]
 
 gts = [251, 967, 862, 548, 204, 678, 750, 998, 568, 416, 630, 270, 40, 653, 360, 721, 178, 158, 300, 914, 34, 
        439, 24, 51, 815, 690, 345, 222, 398, 58]
-
-result = comparer_tri(tri_ameliore, tri_bulles, list)
+print(tri_bulles(list(lst)))
+print(tri_ameliore(list(lst)))
+result = comparer_tri(tri_ameliore, tri_bulles, gts)
